39cc6c562b7368639b6baa4396377021eaed27cc
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-18 14:08:52 -0300

    Add MaiorIdadeDirective for age validation

---
+++ src/app/directives/maior-idade.directive.ts
@@ -0,0 +1,25 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+@Directive({
+  selector: '[maiorIdadeValidator]',
+  //esse trecho de código determina que a diretiva será usada como um validador 
+  providers: [{ 
+    provide : NG_VALIDATORS,
+    useExisting: MaiorIdadeDirective,
+    multi: true
+  }]
+})
+export class MaiorIdadeDirective implements Validator{
+  constructor() { }
+  validate(control: AbstractControl): ValidationErrors | null {
+
+    //Aqui montamos toda a lógica de negocios, temos em maos o ano de nascimento do usuario e vamos criar um novo objeto com o ano atual, isso nos trará a possibilidade de comparar as duas idades por meio de uma subtração. tudo isso no fim para obter o resutado booleano da maioridade, descrito no retorno da função
+    const dataNascimento = control.value
+    const ano = new Date(dataNascimento).getFullYear()
+    const anoAtual = new Date().getFullYear()
+    const idadeUsuario = anoAtual - ano;
+    const maioridade : boolean = (anoAtual - ano) >= 18
+    return maioridade? null : {'maiorIdadeValidator': true}
+  }
+}
